3a3ca8996d74cd59615b27230213c1a83158fb0f
alexbon0000 <alexbon0000@mail.ru>
2024-11-15 16:30:13 +0300

    Добавил блок текста и анимацию

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { DownOutlined } from '@ant-design/icons'
 import './App.css'
 import { useRef } from 'react'
+import 'animate.css';
 
 function App() {
 
@@ -25,7 +26,7 @@ function App() {
       </header>
       <section className='about_me_section' style={{backgroundColor:"rgb(60, 139, 102)"}} ref={aboutMeRef}>
         <div className='info_box'>
-          <div className='myFase'>
+          <div className={`animate__animated animate__backInLeft myFase`}>
             <img src='/me.webp' alt='картинка'/>
           </div>
           <div className='text_about_me'>
@@ -43,8 +44,20 @@ function App() {
         </div>
         
       </section>
-      <section ref={aRef} style={{backgroundColor:"rgb(40, 100, 72)"}}>
-        Content
+      <section className='work_info_section' ref={aRef} style={{backgroundColor:"rgb(40, 100, 72)"}}>
+        <div className='work_info_section_box'>
+          <p>На прежних местах работы занимался:</p>
+          <ul>
+            <li>Разработкой подгружаемых 3д моделей с анимацией (использовал библиотеку Three.js).</li>
+            <li>Разработкой графиков (используя konva.js).</li>
+            <li>Разработкой создания и проведения тестирования.</li>
+            <li>Разработкой приложения создания расписания для групп и распределения нагрузки на преподавателей для учебных заведений.</li>
+            <li>Разработкой приложения для ведения документации с возможностью сформировать отчет.</li>
+          </ul>
+          <p>Также использовал MobX и redux для управления состоянием, axios для связи с бекэндом, Ant Design, React Bootstrap, Material UI для UI. 
+          Есть опыт разработки на react native.</p>
+        </div>
+        
       </section>
       <section style={{backgroundColor:"rgb(60, 139, 102)"}}>
         Content
