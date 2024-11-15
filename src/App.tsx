5a780497f43a159b3ef9b26fadfca4a12202bd26
alexbon0000 <alexbon0000@mail.ru>
2024-11-15 15:02:41 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       <section className='about_me_section' style={{backgroundColor:"rgb(60, 139, 102)"}} ref={aboutMeRef}>
         <div className='info_box'>
           <div className='myFase'>
-            <img src='public/myFace.png' alt='картинка'/>
+            <img src='public/me.webp' alt='картинка'/>
           </div>
           <div className='text_about_me'>
             Привет! 
